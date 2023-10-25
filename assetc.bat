36d98d1b425e874fbec4ee95ba9c136c2d3f674d
BenoitDEKEYN <benoitdkn@gmail.com>
2023-10-25 10:22:39 +0200

    bat files

---
+++ assetc.bat
@@ -0,0 +1,4 @@
+cd C:\Users\BenoitDEKEYN\Desktop\algosup-github-workshop
+bin\harfang\assetc\assetc.exe resources
+
+
---
+++ lua.bat
@@ -0,0 +1,4 @@
+cd C:\Users\BenoitDEKEYN\Desktop\algosup-github-workshop
+bin\lua.exe game_mouse_flight.lua
+
+
